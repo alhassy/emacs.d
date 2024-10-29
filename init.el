5254e13e7e7d3a3aae88e80e8ba39c0b803311c2
Musa Al-hassy <musaalhassy@forwardnetworks.com>
2024-10-29 06:37:59 -0400

    Checkpoint Tue 29 Oct 2024 06:37:59 EDT

--- init.el
+++ init.el
@@ -1234,35 +1234,35 @@ if REMOTE is https://github.com/X/Y then LOCAL becomes ∼/Y."
   :defer nil
   :config (doom-modeline-mode))
 
-  ;; Use minimal height so icons still fit; modeline gets slightly larger when
-  ;; buffer is modified since the "save icon" shows up.  Let's disable the icon.
-  ;; Let's also essentially disable the hud bar, a sort of progress-bar on where we are in the buffer.
-  (setq doom-modeline-height 1)
-  (setq doom-modeline-buffer-state-icon nil)
-  (setq doom-modeline-hud t)
-  (setq doom-modeline-bar-width 1)
+;; Use minimal height so icons still fit; modeline gets slightly larger when
+;; buffer is modified since the "save icon" shows up.  Let's disable the icon.
+;; Let's also essentially disable the hud bar, a sort of progress-bar on where we are in the buffer.
+(setq doom-modeline-height 21)
+(setq doom-modeline-buffer-state-icon nil)
+(setq doom-modeline-hud t)
+(setq doom-modeline-bar-width 1)
 
-  ;; Show 3 Flycheck numbers: “red-error / yellow-warning / green-info”, which
-  ;; we can click to see a listing.
-  ;; If not for doom-modeline, we'd need to use flycheck-status-emoji.el.
-  (setq doom-modeline-checker-simple-format nil)
+;; Show 3 Flycheck numbers: “red-error / yellow-warning / green-info”, which
+;; we can click to see a listing.
+;; If not for doom-modeline, we'd need to use flycheck-status-emoji.el.
+(setq doom-modeline-checker-simple-format nil)
 
-  ;; Don't display the buffer encoding, E.g., “UTF-8”.
-  (setq doom-modeline-buffer-encoding nil)
+;; Don't display the buffer encoding, E.g., “UTF-8”.
+(setq doom-modeline-buffer-encoding nil)
 
-  ;; Inactive buffers' modeline is greyed out.
-  ;; (let ((it "Source Code Pro Light" ))
-  ;;   (set-face-attribute 'mode-line nil :family it :height 100)
-  ;;   (set-face-attribute 'mode-line-inactive nil :family it :height 100))
+;; Inactive buffers' modeline is greyed out.
+;; (let ((it "Source Code Pro Light" ))
+;;   (set-face-attribute 'mode-line nil :family it :height 100)
+;;   (set-face-attribute 'mode-line-inactive nil :family it :height 100))
 
-  ;; A quick hacky way to add stuff to doom-modeline is to add to the mode-line-process list.
-  ;; E.g.:  (add-to-list 'mode-line-process '(:eval (format "%s" (count-words (point-min) (point-max)))))
-  ;; We likely want to add this locally, to hooks on major modes.
+;; A quick hacky way to add stuff to doom-modeline is to add to the mode-line-process list.
+;; E.g.:  (add-to-list 'mode-line-process '(:eval (format "%s" (count-words (point-min) (point-max)))))
+;; We likely want to add this locally, to hooks on major modes.
 
-  (setq doom-modeline-minor-modes t)
-  (use-package minions
+(setq doom-modeline-minor-modes t)
+(use-package minions
 
-    :init (minions-mode 1))
+  :init (minions-mode 1))
 
 ;; If not for doom-modeline, we'd need to use fancy-battery-mode.el.
 (display-battery-mode +1)
@@ -2101,21 +2101,38 @@ Functin Source: https://xenodium.com/emacs-dwim-do-what-i-mean/"
          (region-content (when (region-active-p)
                            (buffer-substring-no-properties (region-beginning)
                                                            (region-end)))))
-    (cond ((and region-content clipboard-url (not point-in-link))
-           (delete-region (region-beginning) (region-end))
-           (insert (org-make-link-string clipboard-url region-content)))
-          ((and clipboard-url (not point-in-link))
-           (insert (org-make-link-string
-                    clipboard-url
-                    (read-string "title: "
-                                 (with-current-buffer (url-retrieve-synchronously clipboard-url)
-                                   (dom-text (car
-                                              (dom-by-tag (libxml-parse-html-region
-                                                           (point-min)
-                                                           (point-max))
-                                                          'title))))))))
-          (t
-           (call-interactively 'org-insert-link)))))
+    (-let [ (gerrit-title gerrit-url) (ai/extract-title-and-url (current-kill 0)) ] 
+      (cond
+       ((and gerrit-title gerrit-url)  (insert (org-make-link-string gerrit-url gerrit-title)))
+       ((and region-content clipboard-url (not point-in-link))
+        (delete-region (region-beginning) (region-end))
+        (insert (org-make-link-string clipboard-url region-content)))
+       ((and clipboard-url (not point-in-link))
+        (insert (org-make-link-string
+                 clipboard-url
+                 (read-string "title: "
+                              (with-current-buffer (url-retrieve-synchronously clipboard-url)
+                                (dom-text (car
+                                           (dom-by-tag (libxml-parse-html-region
+                                                        (point-min)
+                                                        (point-max))
+                                                       'title))))))))
+       (t
+        (call-interactively 'org-insert-link))))))
+
+
+(defun ai/extract-title-and-url (str)
+  "Extract the title and URL from the given string STR, omitting the leading number and tags in brackets."
+  (if (string-match "\\(?:[0-9]+: \\)?\\(?:\\[[^]]+\\] \\)?\\(.*?\\) | \\(https?://[^\"]+\\)" str)
+      (let ((title (match-string 1 str))
+            (url (match-string 2 str)))
+        (list title url))))
+;; example usage
+(when nil
+  (let ((input "12345: [nqe, fix] NQE Regex Syntax: Fix parsing of quantifiers | https://gerrit.local.company.com/c/fwd/+/12345"))
+    (-let [ (title url) (ai/extract-title-and-url input) ] 
+      (message "Title: %s\nURL: %s" title url))))
+
 ;; C-c C-l Org-mode ⇐ HTML:3 ends here
 
 ;; [[file:init.org::*get the pkg][get the pkg:1]]
@@ -2811,5 +2828,12 @@ associated major mode; that's what we aim to do here."
 ;; [[file:init.org::*DONE?][DONE?:1]]
 ;; (find-file "~/.emacs.d/init.org")
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
-(message-box "Done")
+
+
+
 ;; DONE?:1 ends here
+
+
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+(load-file "/Users/musa/Documents/private-goal-getting/planning.el")
